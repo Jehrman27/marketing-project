2eaabfd17849a59b9a739cf6c25d927d98f9b1ba
Jehr_ <jehrman27@gmail.com>
2025-07-25 21:17:33 -0400

    initial setup with React and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="credits" data-gfe-screenshot-exclude="true">
+        A challenge by{" "}
+        <a
+          href="https://www.greatfrontend.com/projects?ref=challenges"
+          target="_blank"
+        >
+          GreatFrontEnd Projects
+        </a>
+        . Built by{" "}
+        <a href="https://www.greatfrontend.com/projects/u/jehr" target="_blank">
+          Jonathan Ehrmantraut
+        </a>
+        .
+      </div>
+    </>
+  );
+}
+
+export default App;
