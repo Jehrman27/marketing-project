300bc221b033d8d3871fce9af773dab29970be59
Jehr_ <jehrman27@gmail.com>
2025-08-08 23:08:44 -0400

    clearer image alt text. import image instead of path. remove unnecessary fragment

--- src/pages/TestimonialCardPage/TestimonialCardPage.tsx
+++ src/pages/TestimonialCardPage/TestimonialCardPage.tsx
@@ -1,10 +1,11 @@
 import { TestimonialCard } from "../../components/TestimonialCard/TestimonialCard";
+import profileImage from "/profile.png";
 
 export const TestimonialCardPage = () => {
   return (
     <div className="flex justify-center mt-25">
       <TestimonialCard
-        avatar="profile.png"
+        avatar={profileImage}
         name="Sarah Dole"
         handle="@sarahdole"
         testimonial="I've been searching for high-quality abstract images for my design
