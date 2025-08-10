57446d2857d4bf6b2d0b6ff710653444e167ac8d
Jehr_ <jehrman27@gmail.com>
2025-08-10 15:28:59 -0400

    Create type for Blog Card component

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -1,30 +1,34 @@
 import BlogImage from "/blog-img.jpg";
 import ArrowRight from "/arrow-right-line.svg";
 
-export const BlogCard = () => {
+export type BlogCardProps = {
+  image: string;
+  badge: string;
+  headerText: string;
+  bodyText: string;
+};
+
+export const BlogCard = ({
+  image,
+  badge,
+  headerText,
+  bodyText,
+}: BlogCardProps) => {
   return (
     <div className="w-85 bg-white rounded-lg shadow-sm">
-      <img src={BlogImage} className="w-full h-72 object-cover rounded-t-lg" />
+      <img src={image} className="w-full h-72 object-cover rounded-t-lg" />
       <div className="flex flex-col gap-3 py-6 px-4">
         <div className="flex flex-col gap-2">
           <div className="w-fit text-sm font-normal text-green-700 bg-green-50 rounded-full py-0.5 px-2 border border-green-200">
-            Interior
+            {badge}
           </div>
-          <h1 className="text-lg font-semibold">
-            Top 5 Living Room Inspirations
-          </h1>
+          <h1 className="text-lg font-semibold">{headerText}</h1>
         </div>
         <div className="flex flex-col gap-6">
-          <p className="text-base font-medium text-neutral-600">
-            Curated vibrants colors for your living, make it pop & calm in the
-            same time.
-          </p>
-          <div className="flex items-center gap-1.5">
-            <button className="text-indigo-700 text-base font-medium">
-              Read more
-            </button>
-            <img src={ArrowRight} className="w-5 h-5" />
-          </div>
+          <p className="text-base font-medium text-neutral-600">{bodyText}</p>
+          <button className="flex  items-center gap-1.5 text-indigo-700 text-base font-medium">
+            Read more <img src={ArrowRight} className="w-5 h-5" />
+          </button>
         </div>
       </div>
     </div>
--- src/pages/BlogCardPage/BlogCardPage.tsx
+++ src/pages/BlogCardPage/BlogCardPage.tsx
@@ -1,9 +1,16 @@
 import { BlogCard } from "../../components/BlogCard/BlogCard";
+import BlogImage from "/blog-img.jpg";
 
 export const BlogCardPage = () => {
   return (
     <div className="flex justify-center mt-50">
-      <BlogCard />
+      <BlogCard
+        image={BlogImage}
+        badge="Interior"
+        headerText="Top 5 Living Room Inspirations"
+        bodyText="Curated vibrants colors for your living, make it pop & calm in the
+            same time."
+      />
     </div>
   );
 };
