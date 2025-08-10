ebb42f892d40b7e4ce4a76e0a9de4effc3705c08
Jehr_ <jehrman27@gmail.com>
2025-08-10 18:35:49 -0400

    desktop styling

--- src/components/HeroSectionSimple/HeroSectionSimple.tsx
+++ src/components/HeroSectionSimple/HeroSectionSimple.tsx
@@ -1,22 +1,22 @@
 export const HeroSectionSimple = () => {
   return (
-    <div className="flex flex-col justify-center bg-white w-full min-h-full rounded-sm shadow-sm">
-      <div className="py-12 md:py-16 px-3 md:px-4 flex flex-col gap-12 md:gap-8">
+    <div className="flex flex-col justify-center bg-white w-full rounded-sm shadow-2xl">
+      <div className="flex flex-col py-12 px-3 gap-12 md:py-16 md:px-4 md:gap-8 lg:flex-row lg:py-24 lg:px-24 lg:items-center">
         <div className="flex flex-col gap-8 md:gap-16">
           <div className="flex flex-col gap-4 md:gap-6">
-            <h2 className="text-4xl md:text-5xl font-semibold">
+            <h2 className="text-4xl font-semibold md:text-5xl lg:text-6xl">
               Well crafted abstract images
             </h2>
-            <p className="text-lg md:text-xl font-normal text-neutral-600">
+            <p className="text-lg font-normal text-neutral-600 md:text-xl">
               High quality abstract images for you projects, wallpaper and
               presentations.
             </p>
           </div>
-          <div className="flex gap-4 md:gap-8">
-            <button className="flex-1 md:flex-none py-3 md:py-4 px-5 md:w-53.25 rounded-sm shadow-sm border-1 border-neutral-200 bg-white text-neutral-900 text-base font-medium">
+          <div className="flex gap-4 md:gap-8 md:w-[458px] lg:w-[383px]">
+            <button className="flex-1 py-3 px-5 rounded-sm shadow-sm border-1 border-neutral-200 bg-white text-neutral-900 text-base font-medium md:py-4">
               Learn more
             </button>
-            <button className="flex-1 md:flex-none py-3 md:py-4 px-5 md:w-53.25 rounded-sm shadow-sm border-1 border-neutral-200 bg-indigo-700 text-white">
+            <button className="flex-1 py-3 px-5 rounded-sm shadow-sm border-1 border-neutral-200 bg-indigo-700 text-white text-base font-medium md:py-4">
               See pricing
             </button>
           </div>
